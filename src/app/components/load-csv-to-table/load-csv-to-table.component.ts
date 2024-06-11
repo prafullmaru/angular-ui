8e0e6300d680b023f858b84a884d447a0beb8522
pranavinfor <pranav.jatla@infor.com>
2024-06-11 12:27:52 +0530

    load-csv-to-table form

--- src/app/components/load-csv-to-table/load-csv-to-table.component.ts
+++ src/app/components/load-csv-to-table/load-csv-to-table.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class LoadCsvToTableComponent {
 
+  ngAfterViewInit() {
+    const form = document.querySelector('#load-csv-to-table') as any;
+    const submitButton = document.querySelector('#load-btn-submit') as any;
+
+    form?.addEventListener('submit', (e: Event) => {
+      e.preventDefault();
+      form.checkValidation();
+      if (form.isValid) {
+        console.info('Form Submitted', {
+          customer: form.querySelector('#customer').value,
+          siteReferenceRequired: form.querySelector('#siteReferenceRequired').checked,
+          mappingGroupName: form.querySelector('#mappingGroupName').value,
+          loaderType: form.querySelector('#loaderType').value,
+          sourceFileType: form.querySelector('#sourceFileType').value,
+          targetFileType: form.querySelector('#targetFileType').value,
+          siteReference: form.querySelector('#siteReference').value,
+          targetTable: form.querySelector('#targetTable').value,
+          targetFileDelimiter: form.querySelector('#targetFileDelimiter').value,
+        });
+      } else {
+        console.error('Form is invalid');
+      }
+    });
+
+    submitButton?.addEventListener('click', () => {
+      form.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }));
+    });
+  }
 }
